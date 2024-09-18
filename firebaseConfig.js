aa51fce04c2cb2d127d900e1d64686d30124d1af
Ikuzwe shema Elie <130182097+shemaikuzwe@users.noreply.github.com>
2024-09-18 20:15:26 +0200

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,11 +4,11 @@ import { getFirestore } from "firebase/firestore";
 import "firebase/storage";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 const firebaseConfig = {
-  apiKey: "AIzaSyDcBDqLPHyRsdqqkbA5O9cF48GLwaE4f7A",
-  projectId: "react-native-c3cf5",
-  authDomain: "react-native-c3cf5.firebaseapp.com",
-  appId: "1:1024110346820:android:18cc0d4ad3ff62f51b3211",
-  storageBucket: "react-native-c3cf5.appspot.com",
+  apiKey: process.env.FIREBASE_API_KEY,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  authDomain: process.env.AUTH_DOMAIN,
+  appId: process.env.FIREBASE_APP_ID,
+  storageBucket:process.env.FIREBASE_STORAGE_BUCKET,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -18,3 +18,4 @@ const auth = initializeAuth(app, {
 const storage = getFirestore(app);
 const db = getFirestore(app);
 export { auth, app, db, storage };
+
